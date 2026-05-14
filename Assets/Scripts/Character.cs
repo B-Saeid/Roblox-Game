c3c1f4573839817e6f733f2deab102bd0385d73c
MyMac <bashier_al-saed@outlook.com>
2026-05-14 19:36:08 +0300

    Lesson 3 Start
      - Moved the camera in the hirarichy to be a child of the character so that it can move/jump with it
      - Added ground with BoxCollider and grass texture
      - Charater Jumping force (200) with capsule collider adjusted to only contain the charater instead of the huge shape

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,20 +5,26 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
     public float speed = 10f;
+    public float jumpForce = 200f;
+    new Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
     }
 
     void Jump()
     {
-        
+        rigidbody.AddForce(0, jumpForce, 0);
     }
 }
